27889bdd502ea653cce77cab55ff9b9573ca3b1f
Yothgewalt <eine.schlacht@gmail.com>
2024-07-31 07:07:41 +0700

    feat: added edge features and types.

---
+++ edge.go
@@ -0,0 +1,73 @@
+// Copyright (c) 2024 Faculty of Industrial Technology and Management, KMUTNB (Provided by FITM Elite)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package grafik
+
+// EdgeOptionFunc represent an alias of function type that
+// modifies the specified edge properties.
+type EdgeOptionFunc func(properties *EdgeProperties)
+
+// EdgeProperties represents the properties of an edge.
+type EdgeProperties struct {
+	weight float64
+}
+
+// WithEdgeWeight sets the edge weight for the specified edge
+// properties in the returned EdgeOptionFunc.
+func WithEdgeWeight(weight float64) EdgeOptionFunc {
+	return func(properties *EdgeProperties) {
+		properties.weight = weight
+	}
+}
+
+// Edge represents an edges in a graph. It contains start and end points.
+type Edge[T comparable] struct {
+	source *Vertex[T] // start point of the edges
+	dest   *Vertex[T] // destination or end point of the edges
+
+	properties EdgeProperties
+}
+
+func NewEdge[T comparable](source *Vertex[T], dest *Vertex[T], opts ...EdgeOptionFunc) *Edge[T] {
+	e := &Edge[T]{
+		source: source,
+		dest:   dest,
+	}
+	for _, opt := range opts {
+		opt(&e.properties)
+	}
+
+	return e
+}
+
+// Source returns edge source vertex
+func (e Edge[T]) Source() *Vertex[T] {
+	return e.source
+}
+
+// Destination returns edge dest vertex
+func (e Edge[T]) Destination() *Vertex[T] {
+	return e.dest
+}
+
+// Weight returns the weight of the edge.
+func (e *Edge[T]) Weight() float64 {
+	return e.properties.weight
+}
